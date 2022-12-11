06ab13e658b602e1c5e9209c1234f23bdf7e3f40
Silviu <pascariu.octavian98@gmail.com>
2022-12-11 19:09:26 +0200

    Removed console logs.

--- client/src/environment/environment.ts
+++ client/src/environment/environment.ts
@@ -3,9 +3,6 @@ const environment = {
   apiUrl: process.env.REACT_APP_API_URL_DEV
 };
 
-console.log('REACT ENV', environment);
-console.log('REACT .env', process.env);
-
 if (process.env.NODE_ENV !== 'development') {
   environment.apiUrl = process.env.REACT_APP_API_URL_PRODUCTION;
 }
